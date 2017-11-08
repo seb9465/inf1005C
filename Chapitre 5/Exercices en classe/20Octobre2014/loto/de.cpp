69059f94bcc1627df296d806fef197d2443f4de9
Sebastien Cadorette <cado.94@me.com>
2017-11-08 11:48:23 -0500

    SC : Suppression des notes de cours.  Conservation des TP.

--- Chapitre 5/Exercices en classe/20Octobre2014/loto/de.cpp
+++
@@ -1,1 +0,0 @@
-// Fonction  jouer aux dés. 2 dés. On lance un dé , on lance  l'autre dé jusqu'à la même face.
--- Chapitre 5/Exercices en classe/compterOccurence/compterOccurence/fonctions.cpp
+++
@@ -1,4 +0,0 @@
-#include "listeFonction.hpp"
-
-void trouverOccurence(string Phrase, int freq[])
-])
